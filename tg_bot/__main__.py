2223f21a65bf098532a75554fad3c7bc8e8b6ca1
akshay-col <67306639+akshay-col@users.noreply.github.com>
2020-07-01 21:31:36 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -20,8 +20,8 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 HELP_STRINGS = """
 Hey there! My name is *{}*.
-I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of \
-the things I can help you with.
+I am anly used for cinema world
+
 
 *Main* commands available:
  - /start: start the bot
